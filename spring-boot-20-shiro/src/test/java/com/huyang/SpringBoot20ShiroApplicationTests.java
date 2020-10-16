b6df6102cc6165d6c72db4babc5a6f400774c3d8
huyang <yhu59@best-inc.com>
2020-10-16 14:21:28 +0800

    搭建shiro

---
+++ spring-boot-20-shiro/src/test/java/com/huyang/SpringBoot20ShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot20ShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
